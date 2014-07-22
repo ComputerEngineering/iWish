56e380e1851fcccd5ba6d3c0ec60c9d4967cbee5
Raffaella <Raffaella@Wonderland>
2014-07-22 12:51:23 +0200

    modificato il manifest

--- NewFolderForUpload/iWish/gen/com/progect/iwish/R.java
+++ NewFolderForUpload/iWish/gen/com/progect/iwish/R.java
@@ -12,20 +12,29 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int topolino=0x7f020001;
+        public static final int image1=0x7f020001;
+        public static final int image2=0x7f020002;
+        public static final int image3=0x7f020003;
+        public static final int image4=0x7f020004;
+        public static final int image5=0x7f020005;
+        public static final int image6=0x7f020006;
+        public static final int topolino=0x7f020007;
     }
     public static final class id {
+        public static final int andiamo=0x7f060003;
         public static final int bt_splash=0x7f060000;
+        public static final int buttonNext=0x7f060002;
+        public static final int imageSwitcher1=0x7f060001;
     }
     public static final class layout {
         public static final int splash=0x7f030000;
+        public static final int tutorial=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int loading=0x7f040001;
     }
-
-     public static final class style {
+    public static final class style {
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
@@ -53,6 +62,4 @@ public final class R {
          */
         public static final int AppTheme=0x7f050001;
     }
-
-
 }
