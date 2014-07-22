c58d48223debee7b59e85ab540be05bdfb033a49
Raffaella <Raffaella@Wonderland>
2014-07-22 13:16:12 +0200

    layout PopUp

--- NewFolderForUpload/iWish/gen/com/progect/iwish/R.java
+++ NewFolderForUpload/iWish/gen/com/progect/iwish/R.java
@@ -27,8 +27,11 @@ public final class R {
         public static final int imageSwitcher1=0x7f060001;
     }
     public static final class layout {
-        public static final int splash=0x7f030000;
-        public static final int tutorial=0x7f030001;
+        public static final int popup_adv3=0x7f030000;
+        public static final int popup_badge_vinta=0x7f030001;
+        public static final int popup_complimenti=0x7f030002;
+        public static final int splash=0x7f030003;
+        public static final int tutorial=0x7f030004;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
