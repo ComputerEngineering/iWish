78ae88f04a80a55d80699355881e21f81296755d
miki <miki@localhost.localdomain>
2014-08-05 13:11:30 +0200

    Creata iWishActivity (manca da definire il sistema per esprimere il desiderio)

--- NewFolderForUpload/iWish/src/iWish_Activity/iWishActivity.java
+++ NewFolderForUpload/iWish/src/iWish_Activity/iWishActivity.java
@@ -1,6 +1,18 @@
 package iWish_Activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.progect.iwish.R;
+
 /**Michela*/
 
-public class iWishActivity {
+public class iWishActivity extends Activity{
 
+	@Override
+	  public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.iwish);
+	
+	}
 }
