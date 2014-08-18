d66d5f880910f5b55dd7f2d312f535a3cfe7b7cf
Raffaella <pivetta.raffaella@gmail.com>
2014-08-19 01:59:36 +0200

    aggiunta bottoni in java

--- NewFolderForUpload/iWish/src/iWish_Activity/ProfileActivity.java
+++ NewFolderForUpload/iWish/src/iWish_Activity/ProfileActivity.java
@@ -5,9 +5,18 @@ import com.progect.iwish.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class ProfileActivity extends Activity{
-
+	private ImageButton bt_option;
+	private ImageButton bt_allarme;
+	private ImageButton bt_calendar;
+	private ImageButton bt_wishlist;
+	private ImageButton bt_statics;
+	private ImageButton bt_friends;
+	private Button bt_make_new_wish_profile;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
