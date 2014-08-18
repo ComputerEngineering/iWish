40cadbb51a8102cfcfb7db190b321db246f4915d
Raffaella <pivetta.raffaella@gmail.com>
2014-08-19 01:42:36 +0200

    bubu

--- NewFolderForUpload/iWish/src/iWish_Activity/ProfileActivity.java
+++ NewFolderForUpload/iWish/src/iWish_Activity/ProfileActivity.java
@@ -1,8 +1,17 @@
 package iWish_Activity;
 //Raffaella
 
+import com.progect.iwish.R;
+
 import android.app.Activity;
+import android.os.Bundle;
 
 public class ProfileActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.profile);
+	}
 
 }
