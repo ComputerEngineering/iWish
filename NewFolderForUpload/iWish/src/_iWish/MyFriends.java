f2efcac604e675b2590e87b9d9df359d02a753fd
Raffaella <pivetta.raffaella@gmail.com>
2014-08-27 14:10:49 +0200

    fatta lìinterfaccia MyFriends e la classe Friends manca naturalmente il cmapo photo

--- NewFolderForUpload/iWish/src/_iWish/MyFriends.java
+++ NewFolderForUpload/iWish/src/_iWish/MyFriends.java
@@ -1,5 +1,14 @@
 package _iWish;
+/**Raffaella*/
 
 public interface MyFriends {
 
+	String getKeyFriend();
+	String getName();
+	String getSurname();
+	int getPoint();
+	void setKeyFriend(String keyUtente);
+	void setName(String name);
+	void setSurname(String surname);
+	void setPoint(int point);
 }
--- NewFolderForUpload/iWish/src/iWish_Friends/Friends.java
+++ NewFolderForUpload/iWish/src/iWish_Friends/Friends.java
@@ -1,11 +1,50 @@
 package iWish_Friends;
 
+import _iWish.MyFriends;
 
-public class Friends {
+
+public class Friends implements MyFriends{
 	private String keyFriends;
-	private String nome;
-	private String cognome;
+	private String name;
+	private String surname;
 	private int point;
 	//TODO manca il campo photo
- 
+
+	@Override
+	public String getKeyFriend() {
+		return keyFriends;
+	}
+	@Override
+	public String getName() {
+		return name;
+	}
+	@Override
+	public String getSurname() {
+		return surname;
+	}
+	@Override
+	public int getPoint() {
+		return point;
+	}
+	@Override
+	public void setKeyFriend(String keyUtente) {
+		this.keyFriends=keyFriends;
+	}
+	@Override
+	public void setName(String name) {
+		this.name=name;
+	}
+	@Override
+	public void setSurname(String surname) {
+		this.surname=surname;
+	}
+	@Override
+	public void setPoint(int point) {
+		this.point=point;
+	}
+	@Override
+	public String toString() {
+		return "Friends [keyFriends=" + keyFriends + ", name=" + name
+				+ ", surname=" + surname + ", point=" + point + "]";
+	}
 }
