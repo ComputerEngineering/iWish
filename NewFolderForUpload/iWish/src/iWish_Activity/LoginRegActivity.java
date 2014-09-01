d06691d99bcde118446e06391681ea18f2c56938
Torcasio <antoniotorcas80@hotmail.com>
2014-09-02 00:16:51 +0200

    ultima prova

--- NewFolderForUpload/iWish/src/iWish_Activity/LoginRegActivity.java
+++ NewFolderForUpload/iWish/src/iWish_Activity/LoginRegActivity.java
@@ -10,7 +10,7 @@ import android.view.View.OnClickListener;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
-/**Antonio funge*/
+/**Antonio */
 
 public class LoginRegActivity extends Activity{
 	private EditText edt_username;
