ef270a8172ac09a5c2e39e29e710dbcb29e106b2
Raffaella <pivetta.raffaella@gmail.com>
2014-09-02 17:44:21 +0200

    Activity e layout Congratulation

--- NewFolderForUpload/iWish/src/iWish_Activity/CongratulationActivity.java
+++ NewFolderForUpload/iWish/src/iWish_Activity/CongratulationActivity.java
@@ -6,21 +6,33 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.progect.iwish.R;
 
 /** Raffaella*/
 
 public class CongratulationActivity extends Activity {
-	private ImageView shortcut;
+	private ImageView imgbt_expressWish;
+	private com.mikhaellopez.circularimageview.CircularImageView civ_user;
+	private com.mikhaellopez.circularimageview.CircularImageView civ_type;
+	private com.mikhaellopez.circularimageview.CircularImageView civ_bmi;
+	private TextView tv_type;
+	private TextView tv_bmi;
+	
 	  @Override
 	  public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.congratulation);
 	    
-	    shortcut = (ImageView)findViewById(R.id.cerchio);
-	    
-	    shortcut.setOnClickListener(new OnClickListener() {
+	    civ_bmi = (com.mikhaellopez.circularimageview.CircularImageView)findViewById(R.id.civ_bmi);
+	    civ_type = (com.mikhaellopez.circularimageview.CircularImageView)findViewById(R.id.civ_type);
+	    civ_user = (com.mikhaellopez.circularimageview.CircularImageView)findViewById(R.id.civ_cerchio);
+	    imgbt_expressWish = (ImageView)findViewById(R.id.img_bt_expressWish);
+	    tv_bmi = (TextView)findViewById(R.id.tv_type);
+	    tv_bmi = (TextView)findViewById(R.id.tv_bmi);
+	   
+	    imgbt_expressWish.setOnClickListener(new OnClickListener() {
 	  		
 	  		@Override
 	  		public void onClick(View v) {
